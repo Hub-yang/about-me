bcac9fe3383966cf67c1640fe3ae26dd9670b801
Hub-yang <18830279823@163.com>
2024-03-17 21:51:05 +0800

    feat: create main html

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,12 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
+  rules: {
+    'no-console': 'off',
+  },
   formatters: {
     css: true,
     html: true,
+    markdown: true,
   },
 })
