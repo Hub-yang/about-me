ea41e5485aec39bd1e6b30012af70c1e02a4422b
Hub-yang <18830279823@163.com>
2024-03-18 14:06:30 +0800

    chore: update build script

--- bin/bin.js
+++ bin/bin.js
@@ -4,36 +4,38 @@
 import process from 'node:process'
 import { resolve } from 'node:path'
 import fs from 'node:fs'
-import { exec } from 'node:child_process'
-// 当前路径
+import AdmZip from 'adm-zip'
+// import { exec } from 'node:child_process'
 
 const cwd = process.cwd()
 const rootPath = resolve(cwd)
 const distPath = resolve(cwd, 'dist')
 
+// 删除dist目录
 if (fs.existsSync(distPath))
   fs.rmSync(distPath, { recursive: true })
 
+// 删除dist.zip
 if (fs.existsSync(`${rootPath}/dist.zip`)) {
   const zipPath = `${rootPath}/dist.zip`
   fs.rmSync(zipPath, { recursive: true })
 }
 
+// 拷贝必要文件到dist目录
 fs.mkdirSync(distPath)
+fs.mkdirSync(`${distPath}/public`)
 const files = ['index.html', 'main.js', 'styles.css']
 files.forEach(file => fs.copyFileSync(`${rootPath}/${file}`, `${distPath}/${file}`))
 
-exec('zip -r dist.zip dist', (error, stdout, stderr) => {
-  if (error) {
-    console.log(`压缩失败:${error}`)
-    process.exit(1)
+// 拷贝public文件到dist
+fs.readdirSync(`${rootPath}/public`).forEach((file) => {
+  if (file.trim()) {
+    const filePath = `${rootPath}/public/${file}`
+    fs.copyFileSync(filePath, `${distPath}/public/${file}`)
   }
-
-  if (stderr) {
-    console.log(`压缩失败:${stderr}`)
-    process.exit(1)
-  }
-
-  console.log('process down')
-  process.exit()
 })
+
+// 压缩
+const zip = new AdmZip()
+zip.addLocalFolder(distPath)
+zip.writeZip(`${rootPath}/dist.zip`)
