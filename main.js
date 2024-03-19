da3a346e293399072f27a641a1cdcb7ea468a393
Hub-yang <18830279823@163.com>
2024-03-19 17:52:24 +0800

    feat: add info card

---
+++ main.js
@@ -0,0 +1,14 @@
+(() => {
+  // links
+  const link_github = document.getElementById('link_one')
+  link_github.onclick = () => window.open('https://github.com/Hub-yang', '_blank')
+
+  const link_twitter = document.getElementById('link_two')
+  link_twitter.onclick = () => window.open('https://twitter.com/mochenghualei', '_blank')
+
+  const link_bilibili = document.getElementById('link_three')
+  link_bilibili.onclick = () => window.open('https://space.bilibili.com/278851804', '_blank')
+
+  const link_youtube = document.getElementById('link_four')
+  link_youtube.onclick = () => window.open('https://www.youtube.com/@HuberyYang', '_blank')
+})()
